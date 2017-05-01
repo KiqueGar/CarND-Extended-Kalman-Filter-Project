f504a5b36d8053e16feb0b41b365209a02b0e3ed
Enrique <enriquegarcis@gmail.com>
2017-05-01 12:50:36 -0500

    Intitialices kf Predicts Updates Calculates RMSE

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,7 +36,26 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  ekf_.F_ = MatrixXd(4,4);
+  ekf_.F_ << 1,0,1,0,
+  			0,1,0,1,
+  			0,0,1,0,
+  			0,0,0,1;
+  // Covariance Matrix (Uncertainity of x)
+  ekf_.P_ = MatrixXd(4,4);
+  
+  ekf_.P_ <<  100, 0, 0, 0,
+  			  0, 100, 0, 0,
+  			  0, 0, 1000, 0,
+  			  0, 0, 0, 1000;
+  // Mapping function Laser
+  H_laser_ << 1, 0, 0, 0,
+  		 	  0, 1, 0, 0;
 
+  // Mapping function Radar (Jacobian)
+  Hj_ << 1, 1, 0, 0,
+  		 1, 1, 0, 0,
+  		 1, 1, 1, 1;
 
 }
 
@@ -67,12 +86,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+    	ekf_.x_(0) = measurement_pack.raw_measurements_(0);
+    	ekf_.x_(1) = measurement_pack.raw_measurements_(1);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
+
       /**
       Initialize state.
       */
+    	float ro = measurement_pack.raw_measurements_(0);
+    	float phi = measurement_pack.raw_measurements_(1);
+    	float ro_dot = measurement_pack.raw_measurements_(2);
+
+    	ekf_.x_(0) =  ro * sin(phi);
+    	ekf_.x_(1) =  ro * cos(phi);
+    	ekf_.x_(2) =  ro_dot * sin(phi);
+    	ekf_.x_(3) =  ro_dot * cos(phi);
+
     }
+    previous_timestamp_ = measurement_pack.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -90,6 +122,25 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
+  // Delta time in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_)/1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+
+  ekf_.F_(0,2)= dt;
+  ekf_.F_(1,3)= dt;
+
+  float d2_= dt*dt;
+  float d3_=d2_*dt/2.0;
+  float d4_=d2_*d2_/4.0;
+
+  float noise_ay = 9;
+  float noise_ax = 9;
+
+  ekf_.Q_= MatrixXd(4,4);
+  ekf_.Q_ << d4_*noise_ax, 0, d3_*noise_ax, 0,
+  			 0, d4_*noise_ay, 0, d3_*noise_ay,
+  			 d3_*noise_ax, 0, d2_*noise_ax, 0,
+  			 0, d3_*noise_ay, 0, d2_*noise_ay;
 
   ekf_.Predict();
 
@@ -103,10 +154,23 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the state and covariance matrices.
    */
 
+
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+
+  	// [X] Calculate Hj
+  	Tools tools;
+  	Hj_= tools.CalculateJacobian(ekf_.x_);
+
+    ekf_.H_= Hj_;
+    ekf_.R_= R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.H_= H_laser_;
+    ekf_.R_= R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
+   
   }
 
   // print the output
--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,23 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+	if (estimations.size()!=ground_truth.size()){
+		//cout << "CalculateRMSE(): Invalid vector sizes!" << endl;
+		rmse << 9999,9999,9999,9999;
+	}
+	//Acumulate Squared residuals
+	for (int i=0; i<estimations.size(); i++){
+		VectorXd residual = estimations[i] - ground_truth[i];
+		residual= residual.array()*residual.array();
+		rmse+=residual;
+	}
+	//Median:
+	rmse/=estimations.size();
+	//Square Root:
+	rmse=rmse.array().pow(0.5);
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +39,19 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+	MatrixXd Hj (3,4);
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	float denom_ = pow(pow(px, 2) + pow(py, 2),0.5);
+	float denom2_ = pow(pow(px, 2) + pow(py, 2),1.5);
+	float nume_ = vx*py - vy*px;
+	float numen_ =   vy*px-vx*py;
+
+	Hj << px/denom_, py/denom_, 0, 0,
+	      -py/pow(denom_, 2), px/pow(denom_,2), 0, 0,
+	      py*nume_/denom2_, px*numen_/denom2_, px/denom_, py/denom_;
+	return Hj;
 }
